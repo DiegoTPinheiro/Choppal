a7d8b54b79537b3d27d6208b90aa2b240958d80f
DiegoTPinheiro <diegotpinheiro@outlook.com>
2022-04-06 12:14:07 -0300

    Initial commit

---
+++ Cardapio.js
@@ -0,0 +1,15 @@
+    function total(){
+		var c1 = parseFloat (document.getElementById("c1").value);
+		var c2 = parseFloat (document.getElementById("c2").value);
+		var c3 = parseFloat (document.getElementById("c3").value);
+		var c4 = parseFloat (document.getElementById("c4").value);
+		var b1 = parseFloat (document.getElementById("b1").value);
+		var b2 = parseFloat (document.getElementById("b2").value);
+		var b3 = parseFloat (document.getElementById("b3").value);
+
+		var total = (c1+c2+c3+c4+b1+b2+b3)
+
+		if (total >= 1)
+				alert ("Voa mlk! Aprovado! Média  " +media)
+
+	}
